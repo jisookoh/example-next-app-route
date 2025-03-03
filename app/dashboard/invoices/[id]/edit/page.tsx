cae642f06562dfaf795abf3723cf0758882168e9
jisookoh <gosjioo0312@naver.com>
2025-03-03 21:14:08 +0900

    Fix: generateMetadata => static

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,15 +4,9 @@ import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { id: string };
-}): Promise<Metadata> {
-  return {
-    title: `Edit Invoice ${params.id}`,
-  };
-}
+export const metadata: Metadata = {
+  title: "Edit Invoice",
+};
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
